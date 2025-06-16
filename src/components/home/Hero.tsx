df134d635aa54d0d247876a44afa01c74ef0996b
AdeyinkaEsi1 <yhamid2828@gmail.com>
2025-06-16 16:38:15 +0100

    [md] add on-entry animation

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,11 +1,18 @@
 import { Typewriter } from 'react-simple-typewriter';
+import { motion } from "framer-motion";
+
 
 export default function Hero() {
     return (
         <>
             <hr className="border-0.5 border-[#FFA500]/70" />
             <section className="flex flex-col items-center justify-center bg-gradient-to-r from-[#000080] to-[#FFA500] text-white py-10 px-6 text-center min-h-screen font-serif">
-                <h1 className="text-4xl sm:text-6xl font-bold mb-4 text-[#FFA500]">
+                <motion.h1
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 2 }}
+                    viewport={{ once: false, amount: 0.2 }}
+                    className="text-4xl sm:text-6xl font-bold mb-4 text-[#FFA500]">
                     Discover the World with {' '}
                     <span className="">
                         <Typewriter
@@ -16,18 +23,29 @@ export default function Hero() {
                             typeSpeed={100}
                             deleteSpeed={50}
                             delaySpeed={2000}
-                            
+
                         />
                     </span>
-                </h1>
+                </motion.h1>
 
-                <p className="text-lg sm:text-xl mb-8 max-w-xl mx-auto text-[#000080]">
+                <motion.p
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ delay: 0.3, duration: 0.6 }}
+                    viewport={{ once: true }}
+                    className="text-lg sm:text-xl mb-8 max-w-xl mx-auto text-[#000080]">
                     Explore breathtaking destinations, tailored just for you.
-                </p>
+                </motion.p>
 
-                <button className="bg-[#000080] text-[#FFA500] px-6 py-3 font-semibold rounded-md shadow-md hover:bg-[#FFA500] hover:text-[#000080] cursor-pointer transition duration-500 hover:scale-110">
+                <motion.button
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ delay: 0.3, duration: 0.6 }}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    className="bg-[#000080] text-[#FFA500] px-6 py-3 font-semibold rounded-md shadow-md hover:bg-[#FFA500] hover:text-[#000080] cursor-pointer transition duration-500 hover:scale-110">
                     Start Your Journey
-                </button>
+                </motion.button>
             </section>
         </>
     );
